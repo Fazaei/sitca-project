47017d047ada4f2b54d05314546e99a304d167b6
mahyar <mahyar@gmail.com>
2025-01-03 17:00:40 +0330

    stracture and confige

---
+++ src/screen/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../../component/landingComponent/heroSection/HeroSection'
+import OurTeam from '../../component/landingComponent/ourTeam/OurTeam'
+
+const Landing = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <OurTeam/>
+    </div>
+  )
+}
+
+export default Landing
